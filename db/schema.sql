dad7277d4fde81d115ab27b4294754a6a40db907
Tracy Le <tnle9@asu.edu>
2019-06-07 21:58:52 -0700

    Edited schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,5 @@
 -- If the table already exists, remove it before trying to create the table again
 DROP TABLE IF EXISTS burgers;
-ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'Ngoc513*';
 
 -- Create the burgers table
 CREATE TABLE burgers (
